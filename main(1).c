f2ad0289d82f91245d7aa4c9b22936f460990c75
Frane6 <frane.zubak6@gmail.com>
2025-07-01 12:55:12 +0200

    main(1).c

--- main(1).c
+++ main(1).c
@@ -1,8 +1,9 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "header.h"
 
-int totalStudents = 0;
+int totalStudents = 0; // [KONCEPT 6] globalna varijabla
 
+// [KONCEPT 11] enum za izbornik
 typedef enum {
     MENU_CREATE = 1,
     MENU_READ,
@@ -16,11 +17,11 @@ typedef enum {
 } MenuOption;
 
 int main(void) {
-    Student* students = NULL;
+    Student *students = NULL; // [KONCEPT 12] pokazivač, početno NULL
     int choice;
 
     do {
-        showMenu();
+        showMenu(); // [KONCEPT 10] izbornik
         if (scanf("%d", &choice) != 1) {
             fprintf(stderr, "Invalid input.\n");
             while (getchar() != '\n');
@@ -28,51 +29,50 @@ int main(void) {
         }
 
         switch (choice) {
-        case MENU_CREATE:
-            createStudent(&students, &totalStudents);
-            break;
-        case MENU_READ:
-            readStudents(students, totalStudents);
-            break;
-        case MENU_UPDATE:
-            updateStudent(students, totalStudents);
-            break;
-        case MENU_DELETE:
-            deleteStudent(students, totalStudents);
-            break;
-        case MENU_SAVE:
-            saveToFile(students, totalStudents);
-            break;
-        case MENU_LOAD:
-            loadFromFile(&students, &totalStudents);
-            break;
-        case MENU_SORT:
-            sortStudents(students, totalStudents);
-            break;
-        case MENU_SEARCH: {
-            int id;
-            printf("Enter ID to search: ");
-            if (scanf("%d", &id) != 1) break;
-            Student* s = searchStudent(students, totalStudents, id);
-            if (s && s->isActive) {
-                printf("%d | %s %s | %s | %.2f | Year %d | %s\n",
-                    s->studentId, s->firstName, s->lastName, s->city,
-                    s->averageGrade, s->currentYear,
-                    s->gender == MALE ? "Male" : s->gender == FEMALE ? "Female" : "Other");
+            case MENU_CREATE:
+                createStudent(&students, &totalStudents); // [KONCEPT 1] C(R)UID - Create
+                break;
+            case MENU_READ:
+                readStudents(students, totalStudents);    // [KONCEPT 1] C(R)UID - Read
+                break;
+            case MENU_UPDATE:
+                updateStudent(students, totalStudents);   // [KONCEPT 1] CR(U)ID - Update
+                break;
+            case MENU_DELETE:
+                deleteStudent(students, totalStudents);   // [KONCEPT 1] CRU(I)D - Delete
+                break;
+            case MENU_SAVE:
+                saveToFile(students, totalStudents);      // [KONCEPT 19] datoteke
+                break;
+            case MENU_LOAD:
+                loadFromFile(&students, &totalStudents);  // [KONCEPT 19, 20, 22]
+                break;
+            case MENU_SORT:
+                sortStudents(students, totalStudents);    // [KONCEPT 23]
+                break;
+            case MENU_SEARCH: {
+                int id;
+                printf("Enter ID to search: ");
+                if (scanf("%d", &id) != 1) break;
+                Student *s = searchStudent(students, totalStudents, id); // [KONCEPT 24]
+                if (s && s->isActive) {
+                    printf("%d | %s %s | %s | %.2f | Year %d | %s\n",
+                        s->studentId, s->firstName, s->lastName, s->city,
+                        s->averageGrade, s->currentYear,
+                        s->gender == MALE ? "Male" : s->gender == FEMALE ? "Female" : "Other");
+                } else {
+                    printf("Student not found.\n");
+                }
+                break;
             }
-            else {
-                printf("Student not found.\n");
-            }
-            break;
-        }
-        case MENU_EXIT:
-            break;
-        default:
-            printf("Invalid choice.\n");
+            case MENU_EXIT:
+                break;
+            default:
+                printf("Invalid choice.\n");
         }
 
     } while (choice != MENU_EXIT);
 
-    freeMemory(&students);
+    freeMemory(&students); // [KONCEPT 18] dealokacija memorije
     return 0;
 }
